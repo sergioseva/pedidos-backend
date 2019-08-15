fddfedddbdf8f6d4ceea8702396348924f9c4b20
Sergio Seva <sergioseva@gmail.com>
2019-08-15 17:15:43 -0300

    Adding test

--- src/test/java/com/librosmario/pedidos/PedidosApplicationTests.java
+++ src/test/java/com/librosmario/pedidos/PedidosApplicationTests.java
@@ -1,5 +1,7 @@
 package com.librosmario.pedidos;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +14,13 @@ public class PedidosApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	
+	
+	@Test
+	public void testDummy() {
+		assertTrue(true);
+	}
+	
+	
 
 }
